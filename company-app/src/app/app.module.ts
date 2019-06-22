07e78d94b5fd6cca6e2cd9187a2fa95343290346
Adrian <adrianc@kainos.com>
2019-06-22 16:24:38 +0100

    simple add employee form

--- company-app/src/app/app.module.ts
+++ company-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEmployeeComponent
   ],
   imports: [
     BrowserModule,
