d19eb2654b4f1f4407ed632cc8bcf0963dfbbe13
BenAAndrew <bandrew01@qub.ac.uk>
2019-06-27 13:50:18 +0100

    pie chart

---
+++ company-app/src/pie.js
@@ -0,0 +1,36 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+    data = document.getElementById('tableData').innerHTML.split("<li ")
+    data.shift()
+    for(var i = 0; i < data.length; i++){
+        data[i] = data[i].split(">")[1]
+        data[i] = data[i].split("<")[0]
+    }
+    names = new Set(data)
+    names = Array.from(names);
+    totals = [['abc','def']]
+    for(var i = 0; i < names.length; i++){
+        var total = 0;
+        for(var j = 0; j < data.length; j++){
+            if(data[j] == names[i]){
+                total++;
+            }
+        }
+        totals[i+1] = [names[i], total];
+    }
+
+    var data = google.visualization.arrayToDataTable(totals);
+
+    var options = {
+    title: 'Staff per department'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+
+    
+}
